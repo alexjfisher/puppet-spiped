3cbb50f9b422085f571ae4f261da2bcbcafd892f
Alexander Fisher <alex@linfratech.co.uk>
2019-06-13 14:47:14 +0100

    Remove redundant resource relationships
    No need to `require` a resource that's also being subscribed to.
    File resources autorequire parent directory resources.

--- manifests/tunnel.pp
+++ manifests/tunnel.pp
@@ -23,12 +23,10 @@ define spiped::tunnel(
       mode      => '0600',
       show_diff => false,
       content   => $secret,
-      require   => File['/etc/spiped'];
   }
 
   exec { "start-spiped-${title}":
     command     => "systemctl daemon-reload && systemctl restart spiped-${title}",
-    require     => File[$unitfile],
     subscribe   => File[$unitfile],
     refreshonly => true;
   }
@@ -36,7 +34,7 @@ define spiped::tunnel(
   service { "spiped-${title}":
     ensure    => running,
     enable    => true,
-    require   => [File[$unitfile], Exec["start-spiped-${title}"]],
+    require   => Exec["start-spiped-${title}"],
     subscribe => File[$unitfile];
   }
 }
